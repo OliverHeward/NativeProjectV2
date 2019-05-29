54a4aa8c6f3e2aa00f15e99159b964304afee0ff
Oliver Heward <oliverhewardbie@gmail.com>
2019-05-29 11:04:59 +0100

    Added more map functionality,  implemented geolocation services to find current location of user, also added a map marker

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -13,33 +13,72 @@ class PickLocation extends Component {
             latitudeDelta: 0.0122,
             longitudeDelta: Dimensions.get("window").width / Dimensions.get("window").height * 0.0122
             // Longitude working out aspect ratio using Dimenions API
-        }
+        },
+        // Initial chosen location is set to false on load
+        locationChosen: false
     }
-
+    // Pick Location Handler running event.
+    // coords set to Native Event Coordinate which is a known command for Map API
     pickLocationHandler = event => {
-        const coords = event.nativeEvent .coordinate;
+        const coords = event.nativeEvent.coordinate;
+        this.map.animateToRegion({
+            // spread operator
+            ...this.state.focusedLocation, 
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        });
         this.setState(prevState => {
+            // PrevState is ran to maintain current state of all keys before Lati & Longi are updated
             return {
                 focusedLocation: {
                     ...prevState.focusedLocation,
                     latitude: coords.latitude,
                     longitude: coords.longitude
-                }
+                },
+                locationChosen: true
+                // on location picked, chosen location is set to true
             };
         })
     }
 
+    getLocationHandler = () => {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const coordsEvent = {
+                nativeEvent: {
+                    coordinate: {
+                        latitude: pos.coords.latitude,
+                        longitude: pos.coords.longitude
+                    }
+                }
+            };
+            this.pickLocationHandler(coordsEvent);
+        },
+        err => {
+            console.log(err);
+            alert("Fetching the position failed, please pick one manually!")
+        })
+    }
+
     render() {
+        // Marker currently set to null whilst locationChosen is false
+        let marker = null;
+        // When location is chosen, set marker to have this JSX
+        if (this.state.locationChosen) {
+            marker = <MapView.Marker coordinate={this.state.focusedLocation} />
+        }
         return( 
         <View style={styles.container}>                 
             <MapView 
+            // Initial Region on screen load
                 initialRegion={this.state.focusedLocation}
-                region={this.state.focusedLocation}
                 style={styles.map}
                 onPress={this.pickLocationHandler}
-            />
+                ref={ref => this.map = ref}
+            >
+            {marker}
+            </MapView> 
             <View style={styles.button}>
-                <Button title="Locate Me" onPress={() => alert('Choose Location')}/>
+                <Button title="Locate Me" onPress={this.getLocationHandler}/>
             </View>
         </View>
         );
